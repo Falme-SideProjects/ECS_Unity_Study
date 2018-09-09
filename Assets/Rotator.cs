de30cd0fb3147bbe8cb4e5021df4440fd6e9e3c8
Falme Streamless <falme_shyness@hotmail.com>
2018-09-08 23:23:43 -0300

    Feito exemplo do Brackeys

---
+++ Assets/Rotator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Rotator : MonoBehaviour {
+
+	
+	//BEFORE
+
+	// // Update is called once per frame
+	// void Update () {
+	// 	transform.Rotate(Vector3.right * velocity * Time.deltaTime);
+	// }
+
+
+	 public float velocity;
+}
+
+class RotatorSystem : ComponentSystem
+{
+
+	struct Components
+	{
+		public Rotator rotator;
+		public Transform transform;
+	}
+
+	protected override void OnUpdate(){
+
+		float dTime = Time.deltaTime;
+
+		var _components = GetEntities<Components>();
+
+		for( int a=0; a<_components.Length; a++ ){
+			_components[a].transform.Rotate(0f, _components[a].rotator.velocity * dTime, 0f);
+		}
+	}
+
+}
