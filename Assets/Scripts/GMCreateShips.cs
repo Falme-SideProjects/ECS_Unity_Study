012ab5f944e384ac06b7a47eeb98dfa2d4e2d622
Falme Streamless <fcarvalho@fluxgamestudio.com>
2018-09-19 01:01:47 -0300

    Reordenando os conhecimentos

---
+++ Assets/Scripts/GMCreateShips.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMCreateShips : MonoBehaviour {
+
+	public int enemyShipIncremement;
+	public GameObject enemyShipPrefab;
+	public float leftBound, rightBound, topBound;
+
+	void Update()
+	{
+		if (Input.GetKeyDown("space"))
+			AddShips(enemyShipIncremement);
+	}
+
+	void AddShips(int amount)
+	{
+		for (int i = 0; i < amount; i++)
+		{
+			float xVal = Random.Range(leftBound, rightBound);
+			float zVal = Random.Range(0f, 10f);
+
+			Vector3 pos = new Vector3(xVal, 0f, zVal + topBound);
+			Quaternion rot = Quaternion.Euler(0f, 180f, 0f);
+
+			var obj = Instantiate(enemyShipPrefab, pos, rot) as GameObject;
+		}
+	}
+
+}
---
+++ Assets/Scripts/ShipBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBehaviour : MonoBehaviour {
+
+	void Update()
+	{
+		Vector3 pos = transform.position;
+		pos += transform.forward * 1f * Time.deltaTime;
+
+		if (pos.z < -500)
+			pos.z = 0;
+
+		transform.position = pos;
+	}
+	
+
+}
